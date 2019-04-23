cf065a89f0b6e9ba9b9dc3da8b961dd4fe94716d
Undying Soul <undyingsoul.mc@gmail.com>
2019-04-23 09:07:19 -0600

    Added readme
    Added readme, a list of languages implemented in this repo.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-Hello-World
+### This repository currently contains "Hello World" programs in the following languages:
+* [1C Enterprise](%23%5C1C-Enterprise)
+* [Actionscript](A%5CActionScript.actionscript)
+* [Zephir](Z%5CZephir.zep)
---
+++ list_langs.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+#Found here: https://github.com/leachim6/hello-world/blob/master/list_langs.py
+
+import os
+from urllib.parse import quote
+
+readme = open('README.md', 'w')
+
+# Copy template to README
+with open('README_nolist.md', 'r') as temp:
+  for line in temp:
+    readme.write(line)
+
+# Write title
+readme.write('### This repository currently contains "Hello World" programs in the following languages:\n')
+
+# List the available languages
+for dirname in sorted(os.listdir('.')):
+  if not (dirname == '.' or dirname == '..' or dirname[0] == '.' or os.path.isfile(dirname)):
+    for filename in sorted(os.listdir(dirname), key=lambda s: s.lower()):
+      if os.path.isfile(os.path.join(dirname, filename)):
+        lang = os.path.splitext(filename)[0].replace('-', ' ').replace('_', ' ').title()
+        readme.write('* [{}]({})\n'.format(lang, quote(os.path.join(dirname, filename))))
+
+readme.close()
